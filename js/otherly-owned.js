e7802228af3b330fbc9d14da79596e315b92e3f7
Arty <perfectashe@gmail.com>
2026-09-15 15:15:20 +0100

    Fixed fetching issue

--- js/otherly-owned.js
+++ js/otherly-owned.js
@@ -111,7 +111,7 @@ function renderCharacters(){
   const slug = char.path.replace(".json","").split("/").pop()
 
   const card = document.createElement("a")
-  card.href = `character-profile.html?char=${slug}`
+  card.href = `otherly-owned-profile.html?char=${slug}`
   card.className = "char-card"
 
   card.innerHTML = `
