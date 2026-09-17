5cb7e5319ba3367b8eb11dcebbdf7034f59ebbd8
Arty <perfectashe@gmail.com>
2026-09-17 15:22:11 +0100

    style

--- js/character-profile-common.js
+++ js/character-profile-common.js
@@ -287,7 +287,6 @@ function applyCharacterTextGradient(character) {
       body.character-profile-page strong,
       body.character-profile-page em,
       body.character-profile-page a,
-      body.character-profile-page button,
       body.character-profile-page .info-label,
       body.character-profile-page .info-value,
       body.character-profile-page .card-name,
