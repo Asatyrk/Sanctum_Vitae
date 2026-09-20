29fd38510f3152f1c7dc1e01213bdc649e937033
Arty <perfectashe@gmail.com>
2026-09-20 22:42:29 +0100

    Pref. original

--- js/breadcrumb.js
+++ js/breadcrumb.js
@@ -200,20 +200,9 @@ function updateBreadcrumb(){
           const content =
             isCurrent
 
-            ? `<span class="breadcrumb-current">${item.name}</span>`
+              ? `<span class="breadcrumb-current">${item.name}</span>`
 
-                : item.name === "Home"
-
-                  ? `
-                    <a class="breadcrumb-link breadcrumb-home" href="${item.href}">
-                      <svg class="breadcrumb-home-icon" aria-hidden="true">
-                        <use href="#icon-home"></use>
-                      </svg>
-                      <span>Home</span>
-                    </a>
-                  `
-
-                  : `<a class="breadcrumb-link" href="${item.href}">${item.name}</a>`
+              : `<a class="breadcrumb-link" href="${item.href}">${item.name}</a>`
 
 
           return (
